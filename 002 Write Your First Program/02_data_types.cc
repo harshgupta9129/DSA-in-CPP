853fda9657b6cbe06594d621dacd29f7c9278fa7
harshgupta9129 <rockstar221405@gmail.com>
2025-05-17 00:33:58 +0530

    First Program Source Code Added

---
+++ 002 Write Your First Program/02_data_types.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int a = 123;
+    float b = 12.4;
+    bool c = false;
+    double d = 12.68;
+    char e = '*';
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
---
+++ 002 Write Your First Program/03_sizeofdatatypes.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int a = 123;
+    float b = 12.4;
+    bool c = false;
+    double d = 12.68;
+    char e = '*';
+    cout<<"Size Of a is "<<sizeof(a)<<endl;
+    cout<<"Size Of b is "<<sizeof(b)<<endl;
+    cout<<"Size Of c is "<<sizeof(c)<<endl;
+    cout<<"Size Of d is "<<sizeof(d)<<endl;
+    cout<<"Size Of e is "<<sizeof(e)<<endl;
+    return 0;
+}
